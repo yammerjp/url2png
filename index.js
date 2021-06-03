a8442eeb8324a3cd6c0752ce91fa64b6f9d394de
Keisuke Nakayama <me@yammer.jp>
2021-06-04 01:39:49 +0900

    Update: simplify

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 const puppeteer = require('puppeteer');
-//const createHash = require("sha256-uint8array").createHash;
-//const calcSha256 = (str) => { return createHash().update(str).digest("hex") };
 const crypto = require('crypto')
 
 const url = process.argv[2]
@@ -12,15 +10,15 @@ console.log(url)
 console.log(urlSha256)
 const main = async () => {
 
-  const browser = await puppeteer.launch();//Chromiumを起動
-  const page = await browser.newPage();//新しいタブを開く
-  await page.goto(url);//指定したURLに移動
-  await page.screenshot({ path: `img/${urlSha256}.png` });//スクリーンショットを撮る
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.screenshot({ path: `img/${urlSha256}.png` });
   console.log(`saved`)
   console.log(`  url : ${url}`)
   console.log(`  file: img/${urlSha256}.png`)
 
-  await browser.close();//Chromiumを閉じる
+  await browser.close();
 };
 
 main();
